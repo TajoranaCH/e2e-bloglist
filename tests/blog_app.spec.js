d38cf8ce1231e6445270836733de2957a76cae6b
Japneet Singh <japneetsingh1807@outlook.it>
2024-06-27 16:57:55 +0200

    more test

--- tests/blog_app.spec.js
+++ tests/blog_app.spec.js
@@ -1,5 +1,5 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
-const { loginWith } = require('./helpers')
+const { loginWith, createBlog } = require('./helpers')
 
 describe('Blog app', () => {
   beforeEach(async ({ page, request  }) => {
@@ -26,9 +26,87 @@ describe('Blog app', () => {
     await expect(locator).toBeVisible()
   })
 
-  test('user can login with correct credentials', async ({ page }) => {
-    await loginWith(page, 'japneet', 'asdasd123')
+  describe('Login', () => {
+    test('user can login with correct credentials', async ({ page }) => {
+      await loginWith(page, 'japneet', 'asdasd123')
+      await expect(page.getByText('Japneet logged in')).toBeVisible()
+    })
+
+    test('login fails with wrong password', async ({ page }) => {
+      await loginWith(page, 'mluukkai', 'wrong')
   
-    await expect(page.getByText('Japneet logged in')).toBeVisible()
+      const errorDiv = await page.locator('.error')
+      await expect(errorDiv).toContainText('wrong credentials')
+      await expect(errorDiv).toHaveCSS('border-style', 'solid')
+      await expect(errorDiv).toHaveCSS('color', 'rgb(255, 0, 0)')
+      await expect(page.getByText('Japneet logged in')).not.toBeVisible()
+    })  
+  })
+
+  describe('when logged in', () => {
+    beforeEach(async ({ page }) => {
+      await loginWith(page, 'japneet', 'asdasd123')
+    })
+
+    test('a new blog can be created', async ({ page }) => {
+      await createBlog(page, 'blog_name', 'blog_author', 'blog:url')
+      await page.waitForTimeout(5000);
+      await expect(page.getByText('blog_name')).toBeVisible()
+    })
+
+    test('a blog can be liked', async ({ page }) => {
+      await createBlog(page, 'blog_name', 'blog_author', 'blog:url')
+      await page.getByRole('button', { name: 'view' }).click()
+      await page.getByRole('button', { name: 'like' }).click()
+      await expect(page.getByText('1')).toBeVisible()
+    })
+  
+    test('a blog can be deleted', async ({ page }) => {
+      await createBlog(page, 'blog_name', 'blog_author', 'blog:url')
+      await page.waitForTimeout(5000);
+      await page.getByRole('button', { name: 'view' }).click()
+      page.on('dialog', dialog => dialog.accept());
+      await page.getByRole('button', { name: 'delete' }).click()
+      await expect(page.getByText('blog_name')).not.toBeVisible()
+    })
+  
+    test('a blog can be deleted only by the creator', async ({ page, request }) => {
+      await request.post('/api/users', {
+        data: {
+          name: 'user2',
+          username: 'user2',
+          password: 'asdasd123'
+        }
+      })
+      await createBlog(page, 'blog_name', 'blog_author', 'blog:url')
+      await page.waitForTimeout(2000);
+      await page.getByRole('button', { name: 'logout' }).click()
+      await loginWith(page, 'user2', 'asdasd123')
+      await page.waitForTimeout(2000);
+      await page.getByRole('button', { name: 'view' }).click()
+      await expect(page.getByText('delete')).not.toBeVisible()
+    })
+
+    test('blogs should be sorted by likes', async ({ page, request }) => {
+      await createBlog(page, 'blog_1_like', 'blog_author', 'blog:url')
+      await createBlog(page, 'blog_2_likes', 'blog_author', 'blog:url')
+      await createBlog(page, 'blog_3_likes', 'blog_author', 'blog:url')
+      await page.waitForTimeout(2000);
+      await page.getByRole('button', { name: 'view' }).last().click()      
+      await page.getByRole('button', { name: 'view' }).last().click()
+      await page.getByRole('button', { name: 'view' }).last().click()
+      const likeButtons = await page.getByRole('button', { name: 'like' }).all()
+      await likeButtons[0].click()
+      await likeButtons[1].click()
+      await likeButtons[1].click()
+      await likeButtons[2].click()
+      await likeButtons[2].click()
+      await likeButtons[2].click()
+      await page.waitForTimeout(4000);
+      const blogs = await page.getByText(/blog_\d_like/).all();
+      await expect(blogs[0]).toContainText('blog_1_like');
+      await expect(blogs[1]).toContainText('blog_2_like');
+      await expect(blogs[2]).toContainText('blog_3_like');
+    })
   })
 })
--- tests/helpers.js
+++ tests/helpers.js
@@ -4,4 +4,13 @@ const loginWith = async (page, username, password)  => {
     await page.getByTestId('password').fill(password)
     await page.getByRole('button', { name: 'login' }).click()
 }
-export { loginWith }
+
+const createBlog = async (page, title, author, url) => {
+    await page.getByRole('button', { name: 'new blog' }).click()
+    const textboxes = await page.getByRole('textbox').all()
+    await textboxes[0].fill(title)
+    await textboxes[1].fill(author)
+    await textboxes[2].fill(url)
+    await page.getByRole('button', { name: 'save' }).click()
+}
+export { loginWith, createBlog }
